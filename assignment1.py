37d10c1b2a90191add5735e21bfd8b7f16224ed8
Triggs-Rut <ruthertett303@gmail.com>
2026-09-15 16:14:02 +0100

    assignment1

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+# Creating a login Page for Youtube
+name = input ("Enter Your Full name: ")
+email = input ("Enter Your Email: ")
+contact =int (input("Enter Your Contact Number: "))
+password = input ("Create Password: ")
+con_password = input ("Confirm Password: ")
+height = float(input("Enter Height: "))
+
+
+
+print("_______________SIGN UP_______________")
+print("USERNAME: " +name)
+print("EMAIL: "+email)
+print("PHONE NUMBER: ",contact)
+print("Height: ", height)
